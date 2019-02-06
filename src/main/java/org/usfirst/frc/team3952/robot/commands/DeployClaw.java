b5898d5f8d3d7caf201c524596ae26dc2f03ea1b
FreshMan3952 <haoyanlgd@gmail.com>
2019-02-06 11:25:12 -0800

    just minor clawDeploy improvement

--- src/main/java/org/usfirst/frc/team3952/robot/commands/DeployClaw.java
+++ src/main/java/org/usfirst/frc/team3952/robot/commands/DeployClaw.java
@@ -4,23 +4,32 @@ import edu.wpi.first.wpilibj.command.*;
 
 import org.usfirst.frc.team3952.robot.*;
 
+
+
 public class DeployClaw extends Command {
+    public boolean isFinished;
+    
+
     public DeployClaw() {
         setInterruptible(false);
     }
 
     @Override
-    protected void initialize() {}
+    protected void initialize() {
+        isFinished = false;
+    }
 
     @Override
     protected void execute() {
-        RobotMap.clawDeploy.setAngle(90);
+        RobotMap.clawDeploy.setAngle(90.0);
+        if(RobotMap.clawDeploy.getAngle() == 90.0){
+            isFinished = true;
+        }
     }
 
     @Override
     protected boolean isFinished() {
-        // ?
-        return true;
+        return isFinished;
     }
 
     @Override
